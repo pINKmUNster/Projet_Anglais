026e2e3a020f2dccf9e63f21051b85b14096bfc8
anthony fioravanti <anthony.fioravanti@telecomnancy.eu>
2015-05-05 19:16:27 +0200

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-
 var express = require('express')
     , bodyParser = require('body-parser')
     , fs = require('fs')
     , app = express()
     , path = require('path')
     , rootPath = path.normalize(__dirname)
-
-app.get("/api/api-docs",function(req,res){
-    var file = rootPath+'/API_EXPLORER/api-docs-model.json';
-    res.sendfile(file);
-});
-
-app.get("/api/*",function(req,res){
-    res.sendfile(rootPath+'/API_EXPLORER' +req.path.substring(4));});
-
-
 
 app.set('port', (process.env.PORT || 5001))
 
